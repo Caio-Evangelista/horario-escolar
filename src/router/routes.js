edea53f9fcbfd8a730b9491669a2dd5e7f41f898
Caio-Evangelista <cfevangelista06@gmail.com>
2021-02-21 16:43:10 -0300

    first commit

---
+++ src/router/routes.js
@@ -0,0 +1,17 @@
+
+const routes = [
+  // {
+  //   path: '/',
+  //   component: () => import('layouts/MainLayout.vue'),
+  //   children: [
+  //     { path: '', component: () => import('pages/Index.vue') }
+  //   ]
+  // },
+
+  // {
+  //   path: '*',
+  //   component: () => import('pages/Error404.vue')
+  // }
+]
+
+export default routes
